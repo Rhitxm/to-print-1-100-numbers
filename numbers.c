4477e1cc5a58d27a3ef297e7dfc94341defc7a88
Rhitxm <shyamasree31@gmail.com>
2026-09-23 18:27:30 +0530

    Update numbers.c

--- numbers.c
+++ numbers.c
@@ -6,3 +6,12 @@ int main() {
     }
     return 0;
 }
+//for printing reverse numbers
+#include <stdio.h>
+
+int main() {
+    for (int i=10; i>=1; i=i-1){
+        printf("%d\n", i);
+    }
+    return 0;
+}
