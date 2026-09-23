103add6dc73dd041da8fec29ff4539ce03cad2bc
Rhitxm <shyamasree31@gmail.com>
2026-09-23 19:04:01 +0530

    Update increment operator.c

--- increment operator.c
+++ increment operator.c
@@ -1,1 +1,22 @@
+// i++ : pre increment operator
+#include <stdio.h>
 
+int main() {
+    int i=1;
+    printf("%d\n", i++);
+    printf("%d\n", i);
+    return 0;
+}
+//output:1
+       //2
+//++i : post increment operator
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    printf("%d\n", ++i);
+    printf("%d\n", i);
+    return 0;
+}
+//output:2
+       //2
