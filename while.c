04837be112248920d54e2651b70285664056bc1d
Rhitxm <shyamasree31@gmail.com>
2026-09-24 15:18:23 +0530

    Update while.c

--- while.c
+++ while.c
@@ -1,1 +1,14 @@
+//some basic functions using while loop
 
+//program to print hello world 5 times using while loop
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    while(i<=5){
+        printf("hello world!!\n");
+        i++;
+    }
+   
+    return 0;
+}
