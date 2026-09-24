809402f7a2e90c8d1f3c862c454c549ccad0bfc7
Rhitxm <shyamasree31@gmail.com>
2026-09-24 15:23:49 +0530

    Update 0-n numbers.c

--- 0-n numbers.c
+++ 0-n numbers.c
@@ -1,1 +1,16 @@
+// to print numbers from 0-n where user gives us the value of n
 
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter number:");
+    scanf("%d", &n);
+    int i=0;
+    while(i<=n){
+        printf("%d\n", i);
+        i++;
+    }
+   
+    return 0;
+}
