4f614958c5ee1961f40a3c849d2f15143546b360
Rhitxm <shyamasree31@gmail.com>
2026-09-24 12:06:23 +0530

    Update decrement operator.c

--- decrement operator.c
+++ decrement operator.c
@@ -1,1 +1,18 @@
 //i-- pre decrement operator
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    printf("%d\n", i--);
+    printf("%d\n", i);
+    return 0;
+}
+// --i post decrement operator
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    printf("%d\n", --i);
+    printf("%d\n", i);
+    return 0;
+}
