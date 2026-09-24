9fe048206fed855b9ca0c03554e0622eb2ca14b6
Rhitxm <shyamasree31@gmail.com>
2026-09-24 15:13:08 +0530

    Implement float number printing from 1.0 to 5.0
    Added a program to print float numbers from 1.0 to 5.0.

--- float.c
+++ float.c
@@ -1,1 +1,11 @@
+//to print numbers float numbers from 0 to 5
 
+#include <stdio.h>
+
+int main() {
+    for (float i = 1.0; i <= 5.0; i++) {
+        printf("%f\n", i);
+    }
+        
+    return 0;
+}
