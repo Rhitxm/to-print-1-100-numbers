5669182a96764a7eaabc4fddc3b356c8a2075221
Rhitxm <shyamasree31@gmail.com>
2026-09-24 16:32:22 +0530

    Update tables.c

--- tables.c
+++ tables.c
@@ -1,1 +1,12 @@
 //to print tables of a given number
+#include <stdio.h>
+
+int main() {
+   int n;
+    printf("input the number here:\n");
+    scanf("%d", &n);
+    int i;
+    for (i=0; i<= 10; i++){
+        printf("%d\n", n*i);
+    }
+}
