408acf00e082c3d08ff9267857adc3dc2b75897d
Rhitxm <shyamasree31@gmail.com>
2026-09-25 19:07:13 +0530

    Add comments to clarify continue statement usage
    Added comments explaining the use of the continue statement.

--- uses of continue statement.c
+++ uses of continue statement.c
@@ -1,1 +1,14 @@
+//skips to the iteration
+//to print numbers from 1-5 except 3
+#include <stdio.h>
 
+int main() {
+    for(int i=1; i<=5; i++){
+        if(i==3){
+            continue;
+        }
+    printf("%d\n", i);
+    }
+    return 0;
+}
+// print all numbers from 1-10 except 6
