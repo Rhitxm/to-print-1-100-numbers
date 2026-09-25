f5bda16feaea6783424afd1944c8a8c57a275563
Rhitxm <shyamasree31@gmail.com>
2026-09-25 19:52:15 +0530

    Update tables.c

--- tables.c
+++ tables.c
@@ -10,3 +10,15 @@ int main() {
         printf("%d\n", n*i);
     }
 }
+//printing reverse tables for a given number
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter your number:\n");
+    scanf("%d", &n);
+    for(int i=10; i>=1; i--){
+      printf("%d\n", n*i);
+  }
+    return 0;
+}
