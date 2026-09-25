927d8567d4264a682f42d3d3e2c16784d25ee730
Rhitxm <shyamasree31@gmail.com>
2026-09-25 12:40:07 +0530

    Update uses of break statement.c

--- uses of break statement.c
+++ uses of break statement.c
@@ -1,1 +1,31 @@
+//stops the code at 2
+#include <stdio.h>
+
+int main() {
+    for(int i=1; i<=5; i++){
+        if(i==3){
+            break;
+        }
+        printf("%d\n", i);
+    }
+    printf("end\n");
+    return 0;
+}
+//keep taking input from the user until he enters an odd number (use do while loop)
+#include <stdio.h>
+
+int main() {
+    int n;
+    do{
+        printf("enter number:\n");
+        scanf("%d", &n);
+
+        if (n%2!=0){
+            break;
+        }
+    }while(1);
+    printf("thank you!!");
+    return 0;
+}
+//keep taking input from user until user enters a number which is a multiple of 7
 
