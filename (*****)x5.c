7563f632711ac130fb5b068639587ffe5707d1e4
Rhitxm <shyamasree31@gmail.com>
2026-09-25 22:16:54 +0530

    Update (*****)x5.c

--- (*****)x5.c
+++ (*****)x5.c
@@ -1,1 +1,9 @@
-
+// printing ***** five times vertically 
+#include <stdio.h>
+int main(){
+    char name[]="*****";
+    for(int i=1; i<=5; i++){
+           printf("%s\n", name);
+       }
+    return 0;
+}
