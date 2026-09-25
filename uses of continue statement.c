599195dcae209ead2a77d2f83a04329c428dc071
Rhitxm <shyamasree31@gmail.com>
2026-09-25 19:07:24 +0530

    Implement continue statement in number printing loop
    Added a loop to print numbers from 1 to 10, skipping 6.

--- uses of continue statement.c
+++ uses of continue statement.c
@@ -12,3 +12,14 @@ int main() {
     return 0;
 }
 // print all numbers from 1-10 except 6
+#include <stdio.h>
+
+int main() {
+    for(int i=1; i<=10; i++){
+        if(i==6){
+            continue;
+        }
+    printf("%d\n", i);
+    }
+    return 0;
+}
