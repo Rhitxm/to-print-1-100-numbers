6840a95b2f49a8c5f82327e9549b38d6bdabbb9e
Rhitxm <shyamasree31@gmail.com>
2026-09-25 11:22:02 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,1 +1,18 @@
+//print the sum of first n natural numbers
+#include <stdio.h>
 
+int main() {
+  int n;
+    printf("enter your number:\n");
+    scanf("%d", &n);
+    int sum=0;
+    int i;
+    for (int i=1; i<=n; i++){
+        sum=sum+i;
+    }
+    printf("%d\n", sum);
+    return 0;
+        
+    
+}
+    
