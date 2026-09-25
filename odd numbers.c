015db4f349d8dd0a3e343662538d6a3ab9867fe7
Rhitxm <shyamasree31@gmail.com>
2026-09-25 19:14:04 +0530

    Update odd numbers.c

--- odd numbers.c
+++ odd numbers.c
@@ -1,1 +1,16 @@
+//taking input from user until user enters an odd number
+//using do while loop
+#include <stdio.h>
 
+int main() {
+    int n;
+    do{
+        printf("enter a number:\n");
+        scanf("%d", &n);
+        if (n%2!=0){
+            break;
+        }
+    }while(1);
+    printf("thank you!");
+    return 0;
+}
